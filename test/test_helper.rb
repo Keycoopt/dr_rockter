f2d6e6305affdc0a49908609b6b7e74d6a909905
Corentin Descamps <corentin.descamps@epitech.eu>
2019-04-29 17:13:02 +0200

    Add minitest reporter and include the default default reporter in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "dr_rockter"
 
 require "minitest/autorun"
+require "minitest/reporters"
+
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
